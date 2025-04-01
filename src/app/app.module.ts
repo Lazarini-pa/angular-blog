d0141b6066b278399a69a54d48631e23efa64e89
lazarini <lazarinipa@gmail.com>
2025-04-01 10:31:46 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule, provideClientHydration, withEventReplay } from '@angular
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuBarComponent } from './component/menu-bar/menu-bar.component';
+import { MenuTitleComponent } from './component/menu-title/menu-title.component';
+import { BigCardComponent } from './component/big-card/big-card.component';
+import { SmallCardComponent } from './component/small-card/small-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuBarComponent,
+    MenuTitleComponent,
+    BigCardComponent,
+    SmallCardComponent
   ],
   imports: [
     BrowserModule,
